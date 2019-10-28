1c4b088f4aa49edd73b0d0881621425b3a68cdaf
proflanky <excelj09@gmail.com>
2019-10-28 21:55:04 +0100

    Add files via upload

---
+++ build/01-variables.tf
@@ -0,0 +1,42 @@
+variable "presentation" {
+    description = "The name of the presentation - used for tagging Azure resources so I know what they belong to"
+    default = "__Presentation__"
+}
+
+variable "location" {
+  description = "The Azure Region in which the resources in this example should exist"
+  default     = "__location__"
+}
+
+variable "ResourceGroupName" {
+  description = "The Resource Group Name"
+   default =   "__ResourceGroupName__"
+}
+
+variable "ssh_public_key" {
+   type         = "string"
+   default      = ""
+   description  = "Public key for aksadmin's SSH access."
+}
+
+variable "agent_count" {
+   description  = "The number of Nodes to provision"
+   default =  "__agent_count__"
+}
+
+variable "VMSize" {
+  description = "The size of the VM such as Standard_DS1_v2"
+   default =   "__VMSize__"
+}
+variable "ClusterName" {
+  description = "The name of the cluster"
+   default =   "__ClusterName__"
+}
+variable "AgentPoolName" {
+  description = "The name of the AgentPool"
+   default =   "__AgentPoolName__"
+}
+variable "ServiceName" {
+  description = "The name of the ServiceName"
+   default =   "__ServiceName__"
+}
---
+++ build/02-main.tf
@@ -0,0 +1,160 @@
+provider "azurerm" {
+  version = "=1.24.0"
+}
+
+terraform {
+  backend "azurerm" {
+    key = "terraform.tfstate"
+  }
+}
+
+locals {
+  cluster_name = "${var.ClusterName}-${random_string.aks.result}"
+}
+
+module "service_principal" {
+  source  = "service_principal"
+  sp_name = "${local.cluster_name}"
+}
+
+resource "azurerm_resource_group" "aks" {
+  name     = "${var.ResourceGroupName}"
+  location = "${var.location}"
+}
+
+resource "random_string" "aks" {
+  length  = 4
+  lower   = true
+  number  = true
+  upper   = true
+  special = false
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${local.cluster_name}"
+  location            = "${azurerm_resource_group.aks.location}"
+  resource_group_name = "${azurerm_resource_group.aks.name}"
+  dns_prefix          = "${local.cluster_name}"
+
+  depends_on = [
+    "module.service_principal",
+  ]
+
+  agent_pool_profile {
+    name            = "${var.AgentPoolName}"
+    count           = "${var.agent_count}"
+    vm_size         = "${var.VMSize}"
+    os_type         = "Linux"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = "${module.service_principal.client_id}"
+    client_secret = "${module.service_principal.client_secret}"
+  }
+
+  tags = {
+    Environment = "${var.presentation}"
+  }
+}
+
+resource "kubernetes_namespace" "Dev" {
+  metadata {
+    labels {
+      mylabel = "Place_for_Dev"
+    }
+
+    name = "dev"
+  }
+}
+
+resource "kubernetes_namespace" "Test" {
+  metadata {
+    labels {
+      mylabel = "Place_for_Test"
+    }
+
+    name = "test"
+  }
+}
+
+resource "kubernetes_namespace" "Prod" {
+  metadata {
+    labels {
+      mylabel = "Place_for_Prod"
+    }
+
+    name = "prod"
+  }
+}
+
+resource "kubernetes_deployment" "sql2019" {
+  metadata {
+      namespace = "${kubernetes_namespace.Dev.metadata.0.name}"
+    name = "sql2019deployment"
+    labels {
+      app = "SQL"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels {
+         app = "SQL"
+      }
+    }
+
+    template {
+      metadata {
+        
+        labels {
+           app = "SQL"
+        }
+      }
+
+  spec {
+    container {
+      image = "mcr.microsoft.com/mssql/server:2019-CTP2.4-ubuntu"
+      name  = "sql2019"
+
+      env = {
+        name  = "SA_PASSWORD"
+        value = "Password0!"
+      }
+
+      env = {
+        name  = "ACCEPT_EULA"
+        value = "Y"
+      }
+
+      port {
+        container_port = 1433
+      }
+    }
+  }
+    }
+  }
+}
+resource "kubernetes_service" "sqlserver2019" {
+  metadata {
+    name      = "${var.ServiceName}"
+    namespace = "${kubernetes_namespace.Dev.metadata.0.name}"
+  }
+
+  spec {
+    selector {
+      app = "${kubernetes_deployment.sql2019.metadata.0.labels.app}"
+    }
+
+    session_affinity = "ClientIP"
+
+    port {
+      port        = 1433
+      target_port = 1433
+    }
+
+    type = "LoadBalancer"
+  }
+}
